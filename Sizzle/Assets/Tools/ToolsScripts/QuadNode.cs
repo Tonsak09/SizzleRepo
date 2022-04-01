294ad4d4b1bcd3067db9a96cac3e70195b48d0df
Narai Risser <naraiguy@gmail.com>
2022-03-31 23:14:01 -0400

    More progress on quadnode

--- Sizzle/Assets/Tools/ToolsScripts/QuadNode.cs
+++ Sizzle/Assets/Tools/ToolsScripts/QuadNode.cs
@@ -43,64 +43,163 @@ public class QuadNode : MonoBehaviour
     {
         // Get all objects within the box 
         Collider[] collisions = Physics.OverlapBox(this.transform.position, this.transform.localScale, this.transform.rotation, mask);
-
-        Add(collisions);
-    }
-
-    public void Add(Collider[] collisions)
-    {
-        // For each object get its verticies to see if box contains it 
         foreach (Collider col in collisions)
         {
-            Vector3[] vertices = col.GetComponent<Mesh>().vertices;
+            // Get verticies that actually exist 
+            Add(ProjectToPlane(col.GetComponent<Mesh>().vertices));
+        }
+    }
 
-            if (shapes.Count + 1 <= MAXOBJBEFOREDIVIDE)
+    public void Add(List<Vector3> vertices)
+    {
+        // For each object get its verticies to see if box contains it 
+        
+        if (shapes.Count + 1 <= MAXOBJBEFOREDIVIDE)
+        {
+            shapes.Add(vertices);
+        }
+        else
+        {
+            if (divisions == null)
             {
-                shapes.Add(ProjectToPlane(vertices));
+                // Saves code and apart of group by default anyway 
+                shapes.Add(vertices);
+
+                // Make divisions 
+                Divide();
             }
             else
             {
-                if (divisions == null)
+                // Check divisions for smallest that can be applied 
+
+                // If null or self add to this node 
+
+                // else add to the child 
+            }
+        }
+        
+    }
+
+    private void Divide()
+    {
+        divisions = new QuadNode[4];
+
+        // Works because it comes from a unit cube 
+        Vector3 root = this.transform.position - (this.transform.localScale / 2);
+
+        // Split up and allign position with vector from pos[0]
+        for (int x = 0; x < 2; x++)
+        {
+            for (int y = 0; y < 2; y++)
+            {
+
+                // Centers new node in middle of quadrant but same z axis
+                GameObject nodeTemp = Instantiate(this.gameObject, root +
+                    new Vector3
+                    (
+                        x * (this.transform.localScale.x / 2) + (this.transform.localScale.x / 4), 
+                        y * (this.transform.localScale.y / 2) +(this.transform.localScale.y / 4),
+                        0 // No change from root z
+                    ), 
+                    Quaternion.identity
+                );
+
+                // Does not need to affect z axis**** 
+                nodeTemp.transform.localScale /= 2;
+
+                divisions[x % 1 + y / 2] = nodeTemp.GetComponent<QuadNode>();
+            }
+        }
+
+        foreach (List<Vector3> shape in shapes)
+        {
+            if (PlaneContainsShape(shape))
+            {
+                QuadNode smallest = SmallestNode(shape);
+
+                if (smallest != null || smallest != this)
                 {
-                    // Saves code and apart of group by default anyway 
-                    shapes.Add(ProjectToPlane(vertices));
-
-                    // Make divisions 
-
-                    // Check smallest node that can be applied 
-                }
-                else
-                {
-                    // Check divisions for smallest that can be applied 
-
-                    // If null or self add to this node 
-
-                    // else add to the child 
+                    smallest.Add(shape);
                 }
             }
+            
+            // Already apart of group so need to re-add 
         }
     }
 
-    public bool BoxContains(List<Vector3> verticies)
+    /// <summary>
+    /// Gets the smallest node that can conatin the given vertices 
+    /// </summary>
+    /// <param name="vertices"></param>
+    /// <returns></returns>
+    public QuadNode SmallestNode(List<Vector3> vertices)
     {
-        foreach (Vector3 vertex in verticies)
+        if (PlaneContainsShape(vertices))
         {
-            // If the distance from the position is less
-            // that scale in each axis it's within 
+            if (divisions != null)
+            {
+                // Iterate through each child to see if can fit into any of these
+                foreach (QuadNode child in divisions)
+                {
+                    if (child.PlaneContainsShape(vertices))
+                    {
+                        return SmallestNode(vertices);
+                    }
+                }
 
-            // Todo -> Account for rotation 
+                // No child can fit 
+                return this;
+            }
+            else
+            {
+                return this;
+            }
         }
+
+        return null;
+    }
+
+
+    /// <summary>
+    /// If a point is within the plane formed by this quad.
+    /// </summary>
+    /// <param name="point"></param>
+    /// <returns></returns>
+    public bool PlaneContainsPoint(Vector3 point)
+    {
+        // See if within bounds of points 
 
         return true;
     }
 
+    /// <summary>
+    /// Checks if a series of vertexes fit into a 
+    /// </summary>
+    /// <param name="shape"></param>
+    /// <returns></returns>
+    public bool PlaneContainsShape(List<Vector3> shape)
+    {
+
+        return true;
+    }
+
+    /// <summary>
+    /// Simplifies the vertices to a represent a shape along the up most
+    /// plane. If vertices go out of up most plane they are cut 
+    /// </summary>
+    /// <param name="verticies"></param>
+    /// <returns></returns>
     private List<Vector3> ProjectToPlane(Vector3[] verticies)
     {
         List<Vector3> projections = new List<Vector3>();
 
         foreach (Vector3 vertex in verticies)
         {
+            if(PlaneContainsPoint(vertex))
+            {
+                projections.Add(Vector3.ProjectOnPlane(vertex, plane.normal));
 
+            }
         }
         // Add each point that lies within the plane 
 
@@ -108,4 +207,6 @@ public class QuadNode : MonoBehaviour
 
         return projections;
     }
+
+    
 }
